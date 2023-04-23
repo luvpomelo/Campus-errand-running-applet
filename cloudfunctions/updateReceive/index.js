1af206f11c9a8da272faf9f0d83eb0bce502a6af
ronin_26 <3176240781@qq.com>
2023-04-23 09:57:55 +0800

    接单未测试， 删除订单完成

---
+++ cloudfunctions/updateReceive/index.js
@@ -0,0 +1,22 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init({ env: cloud.DYNAMIC_CURRENT_ENV }) // 使用当前云环境
+const db = cloud.database();
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  const { _id, receivePersion, state } = event;
+  try {
+    return await db.collection('order').doc(_id).update({
+      data: {
+        receivePersion,
+        state,
+      }
+    })
+
+  } catch (error) {
+    console.log(error);
+
+  }
+}
